ad36cd21e37cc292d593e6c512347c8749b34329
Yesdevelop <yes-noob@outlook.com>
2024-03-02 13:16:48 +0800

    稍微重构代码，准备写UI部分了

--- index.js
+++ index.js
@@ -1,11 +1,342 @@
-import board from "./xiangqi.js"
+const rook = 1
+const knight = 2
+const bishop = 3
+const guard = 4
+const king = 5
+const cannon = 6
+const pawn = 7
+const red = 1
+const black = -1
 
-alert("loading")
+/**
+ * 判断是否是红方
+ */
+function isRed(id) {
+  return id > 0
+}
 
-try {
-  board.mount("board")
-  alert("OK")
+/**
+ * 构造位置
+ */
+function pos(x, y) {
+  return { x, y }
 }
-catch(err) {
-  alert(err)
+
+/**
+ * 是否在王区内
+ * @param {number} team 
+ * @returns 
+ */
+function isInKingzone(x, y, team) {
+  if (team === red) return x >= 3 && x <= 5 && y >= 0 && y <= 2
+  else return x >= 3 && x <= 5 && y >= 7 && y <= 9
 }
+
+/**
+ * 是否在家门内
+ */
+function isInHome(x, y, team) {
+  if (team === red) return x >= 0 && x <= 8 && y >= 0 && y <= 4
+  else return x >= 0 && x <= 8 && y >= 5 && y <= 9
+}
+
+/**
+ * 是否在棋盘内
+ */
+function isInBoard(x, y) {
+  x >= 0 && x <= 8 && y >= 0 && y <= 9
+}
+
+
+/**
+ * 棋盘类
+ * 仅实现核心功能，ui部分需要constructor参数填写
+ * @param {Object} view 装着所有ui方法
+ */
+class Board {
+  isRedTurn = true
+  moves = []
+  chessMap = [
+    [rook * red, 0, 0, pawn * red, 0, 0, pawn * black, 0, 0, rook * black],
+    [knight * red, 0, cannon * red, 0, 0, 0, 0, cannon * black, 0, knight * black],
+    [bishop * red, 0, 0, pawn * red, 0, 0, pawn * black, 0, 0, bishop * black],
+    [guard * red, 0, 0, 0, 0, 0, 0, 0, 0, guard * black],
+    [king * red, 0, 0, pawn * red, 0, 0, pawn * black, 0, 0, king * black],
+    [guard * red, 0, 0, 0, 0, 0, 0, 0, 0, guard * black],
+    [bishop * red, 0, 0, pawn * red, 0, 0, pawn * black, 0, 0, bishop * black],
+    [knight * red, 0, cannon * red, 0, 0, 0, 0, cannon * black, 0, knight * black],
+    [rook * red, 0, 0, pawn * red, 0, 0, pawn * black, 0, 0, rook * black],
+  ]
+
+  constructor(view) {
+    this.view = view
+  }
+
+  // Utils
+
+  /** 获取指定位置上的棋子 */
+  chessOn(x, y) {
+    if (this.chessMap[x]) {
+      return this.chessMap[x][y]
+    }
+  }
+
+  /** 获取指定位置上的棋子的队伍 */
+  teamOn(x, y) {
+    return isRed(this.chessOn(x, y)) ? red : black
+  }
+
+  /** 计算是否可以从位置一走棋到位置二 */
+  canMove(x1, y1, x2, y2) {
+    if (isInBoard(x2, y2) === false) {
+      return false
+    }
+    let chess = this.chessOn(x2, y2)
+    if (chess === 0) {
+      return true
+    }
+    let team1 = this.teamOn(x1, y1)
+    let team2 = this.teamOn(x2, y2)
+    if (team2 !== team1) {
+      return true
+    }
+    return false
+  }
+
+  /** 获取指定位置上的棋子的所有可行着法 */
+  getMovesOfChess(x, y) {
+    let team = this.teamOn(x, y)
+    let chess = Math.abs(this.chessOn(x, y))
+
+    switch (chess) {
+      case king:
+        return this.king(x, y)
+      case guard:
+        return this.guard(x, y)
+      case bishop:
+        return this.bishop(x, y)
+      case knight:
+        return this.knight(x, y)
+      case pawn:
+        return this.pawn(x, y)
+      case rook:
+        return this.rook(x, y)
+      case cannon:
+        return this.cannon(x, y)
+    }
+  }
+
+  // ChessMoveMethods
+
+  /** 帅 */
+  king(x, y) {
+    const team = this.teamOn(x, y)
+    let result = [
+      pos(x + 1, y), pos(x - 1, y),
+      pos(x, y + 1), pos(x, y - 1)
+    ]
+    result = result.filter((v) => isInKingzone(v.x, v.y, team) && this.canMove(x, y, v.x, v.y))
+    // 白脸将
+    let count = 0
+    this.chessMap[x].forEach((chess, y_) => {
+      if (chess) {
+        count++
+      }
+      if (Math.abs(chess) === king && y_ !== y && count <= 2) {
+        result.push(pos(x, k))
+      }
+    })
+    return result
+  }
+
+  /** 士 */
+  guard(x, y) {
+    const team = this.teamOn(x, y)
+    let result = [
+      pos(x + 1, y + 1), pos(x + 1, y - 1),
+      pos(x - 1, y + 1), pos(x - 1, y - 1)
+    ]
+    result = result.filter((v) => isInKingzone(v.x, v.y, team) && this.canMove(x, y, v.x, v.y))
+
+    return result
+  }
+
+  /** 相 */
+  bishop(x, y) {
+    const team = this.teamOn(x, y)
+    let result = []
+
+    if (this.chessOn(x + 1, y + 1) === 0 && this.canMove(x, y, x + 2, y + 2))
+      result.push(pos(x + 2, y + 2))
+    if (this.chessOn(x + 1, y - 1) === 0 && this.canMove(x, y, x + 2, y - 2))
+      result.push(pos(x + 2, y - 2))
+    if (this.chessOn(x - 1, y + 1) === 0 && this.canMove(x, y, x - 2, y + 2))
+      result.push(pos(x - 2, y + 2))
+    if (this.chessOn(x - 1, y - 1) === 0 && this.canMove(x, y, x - 2, y - 2))
+      result.push(pos(x - 2, y - 2))
+
+    result = result.filter((v) => isInHome(v.x, v.y, team))
+
+    return result
+  }
+
+  /** 马 */
+  knight(x, y) {
+    let result = []
+
+    if (this.chessOn(x + 1, y) === 0) {
+      result.push(pos(x + 2, y + 1))
+      result.push(pos(x + 2, y - 1))
+    }
+    if (this.chessOn(x - 1, y) === 0) {
+      result.push(pos(x - 2, y + 1))
+      result.push(pos(x - 2, y - 1))
+    }
+    if (this.chessOn(x, y + 1) === 0) {
+      result.push(pos(x + 1, y + 2))
+      result.push(pos(x - 1, y + 2))
+    }
+    if (this.chessOn(x, y - 1) === 0) {
+      result.push(pos(x + 1, y - 2))
+      result.push(pos(x - 1, y - 2))
+    }
+
+    return result.filter((v) => this.canMove(x, y, v.x, v.y))
+  }
+
+  /** 兵 */
+  pawn(x, y) {
+    const team = this.teamOn(x, y)
+    if (isInHome(x, y, team)) {
+      if (team === red) {
+        return [pos(x, y + 1)].filter((v) => this.canMove(x, y, v.x, v.y))
+      }
+      else {
+        return [pos(x, y - 1)].filter((v) => this.canMove(x, y, v.x, v.y))
+      }
+    }
+    else {
+      if (team === red) {
+        return [pos(x + 1, y), pos(x - 1, y),
+        pos(x, y + 1)].filter((v) => this.canMove(x, y, v.x, v.y))
+      }
+      else {
+        return [pos(x + 1, y), pos(x - 1, y),
+        pos(x, y - 1)].filter((v) => this.canMove(x, y, v.x, v.y))
+      }
+    }
+  }
+
+  /** 车 */
+  rook(x, y) {
+    let result = []
+    let x_ = x
+    let y_ = y
+    let rookMove = (x_, y_) => {
+      if (this.chessOn(x_, y_) === 0) {
+        result.push(pos(x_, y_))
+      }
+      else if (this.canMove(x, y, x_, y_)) {
+        result.push(pos(x_, y_))
+        return 1
+      }
+      else {
+        return 1
+      }
+    }
+    while (x_ < 8) {
+      x_++
+      if (rookMove(x_, y) === 1) break
+    }
+    x_ = x
+    while (x_ > 0) {
+      x_--
+      if (rookMove(x_, y) === 1) break
+    }
+    while (y_ < 9) {
+      y_++
+      if (rookMove(x, y_) === 1) break
+    }
+    y_ = y
+    while (y_ > 0) {
+      y_--
+      if (rookMove(x, y_) === 1) break
+    }
+    return result
+  }
+
+  /** 炮 */
+  cannon(x, y) {
+    const team = this.teamOn(x, y)
+    let result = []
+    let x_ = x
+    let y_ = y
+    while (x_ < 8) {
+      x_++
+      if (this.chessOn(x_, y) === 0) {
+        result.push(pos(x_, y))
+      } else {
+        while (x_ < 8) {
+          x_++
+          if (this.teamOn(x_, y) !== team && this.chessOn(x_, y) !== 0) {
+            result.push(pos(x_, y))
+            break
+          }
+        }
+        break
+      }
+    }
+    x_ = x
+    while (x_ > 0) {
+      x_--
+      if (this.chessOn(x_, y) === 0) {
+        result.push(pos(x_, y))
+      } else {
+        while (x_ > 0) {
+          x_--
+          if (this.teamOn(x_, y) !== team && this.chessOn(x_, y) !== 0) {
+            result.push(pos(x_, y))
+            break
+          }
+        }
+        break
+      }
+    }
+    while (y_ < 9) {
+      y_++
+      if (this.chessOn(x, y_) === 0) {
+        result.push(pos(x, y_))
+      } else {
+        while (y_ < 9) {
+          y_++
+          if (this.teamOn(x, y_) !== team && this.chessOn(x, y_) !== 0) {
+            result.push(pos(x, y_))
+            break
+          }
+        }
+        break
+      }
+    }
+    y_ = y
+    while (y_ > 0) {
+      y_--
+      if (this.chessOn(x, y_) === 0) {
+        result.push(pos(x, y_))
+      } else {
+        while (y_ > 0) {
+          y_--
+          if (this.teamOn(x, y_) !== team && this.chessOn(x, y_) !== 0) {
+            result.push(pos(x, y_))
+            break
+          }
+        }
+        break
+      }
+    }
+    return result
+  }
+}
+
+const board = new Board({
+  // TODO
+})
